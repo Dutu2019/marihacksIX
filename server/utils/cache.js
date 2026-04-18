7d3885c86671de4a12bfad124437deaf43300006
Eric Tanase <erictanase2007@gmail.com>
2026-04-17 23:44:54 -0400

    initial commit

--- server/utils/cache.js
+++
@@ -1,9 +0,0 @@
-import NodeCache from 'node-cache';
-import { createHash } from 'crypto';
-
-const CACHE_TTL = parseInt(process.env.CACHE_TTL_SECONDS) || 900;
-export const cache = new NodeCache({ stdTTL: CACHE_TTL, checkperiod: 60 });
-
-export function getCacheKey(type, query) {
-  return `${type}:${createHash('md5').update(query.toLowerCase().trim()).digest('hex')}`;
-}
--- server/utils/logger.js
+++
@@ -1,9 +0,0 @@
-export function log(...args) {
-  const timestamp = new Date().toISOString().split('T')[1].slice(0, 8);
-  console.log(`[${timestamp}]`, ...args);
-}
-
-export function logError(...args) {
-  const timestamp = new Date().toISOString().split('T')[1].slice(0, 8);
-  console.error(`[${timestamp}]`, ...args);
-}
